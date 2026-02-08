375b884f605f2c586682e00c0af27815f2102c30
aidar <anged@mail.ru>
2026-02-08 15:21:27 +0300

    add subtemplete

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,7 +6,7 @@ app_name = CatalogConfig.name
 
 urlpatterns = [
     path('home/', views.home, name='home'),
-    path('', views.home, name='home'),
+    path('', views.products_list, name='products_list'),
     path('product_detail/<int:product_id>/', views.product_detail, name='product_detail'),
     path('products_list/', views.products_list, name='products_list'),
 
