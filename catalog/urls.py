6e4e7ee2b52bb1584d0cfb5f3c4746d46debc4d8
aidar <anged@mail.ru>
2026-02-11 16:34:30 +0300

    catalog product FBV to CBV

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
 from . import views
+from .views import ProductDetailView, ProductListView, ProductCreateView, ContactView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('home/', views.home, name='home'),
-    path('', views.products_list, name='products_list'),
-    path('product_detail/<int:product_id>/', views.product_detail, name='product_detail'),
-    path('products_list/', views.products_list, name='products_list'),
-    path('add_product/', views.add_product, name='add_product'),
-    path('contacts/', views.contacts, name='contacts'),
+    path('', ProductListView.as_view(), name='products_list'),
+    path('product_detail/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('products_list/', ProductListView.as_view(), name='products_list'),
+    path('add_product/', ProductCreateView.as_view(), name='add_product'),
+    path('contacts/', ContactView.as_view(), name='contacts'),
 
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .models import Product
-from django.core.paginator import Paginator
 from django import forms
+
+from django.views.generic import DetailView, ListView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from django.views import View
 
 
 def home(request):
@@ -17,34 +21,28 @@ def home(request):
     return render(request, 'catalog/home.html', {'products': latest_products})
 
 
-def contacts(request):
-    if request.method == 'POST':
+class ContactView(View):
+    def get(self, request):
+        return render(request, 'catalog/contacts.html')
+
+    def post(self, request):
         name = request.POST.get('name')
         message = request.POST.get('message')
+        print(f"Сообщение от {name}: {message}")
         return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
-    return render(request, 'catalog/contacts.html')
 
 
-def product_detail(request, product_id):
-    product = Product.objects.get(id=product_id)
-    context = {
-        'product': product,
-    }
-    return render(request, 'catalog/product_detail.html', context=context)
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'catalog/product_detail.html'
+    context_object_name = 'product'
 
 
-def products_list(requests):
-    products = Product.objects.all()
-
-    paginator = Paginator(products, 2)
-    page_number = requests.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-
-    context = {
-        'products': products,
-        'page_obj': page_obj,
-    }
-    return render(requests, 'catalog/products_list.html', context=context)
+class ProductListView(ListView):
+    model = Product
+    template_name = 'catalog/products_list.html'
+    context_object_name = 'products'
+    paginate_by = 2
 
 
 class ProductForm(forms.ModelForm):
@@ -53,9 +51,8 @@ class ProductForm(forms.ModelForm):
         fields = ['name', 'description', 'image', 'category', 'price']
 
 
-def add_product(request):
-    form = ProductForm(request.POST or None, request.FILES or None)
-    if request.method == "POST" and form.is_valid():
-        form.save()
-        return redirect('/')
-    return render(request, 'catalog/add_product.html', {'form': form})
+class ProductCreateView(CreateView):
+    model = Product
+    fields = ['name', 'description', 'image', 'category', 'price']
+    template_name = 'catalog/product_form.html'
+    success_url = reverse_lazy('catalog:products_list')
